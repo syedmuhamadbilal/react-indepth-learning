440b2fdd9be9768662eb206d3ee6dcd803c46b18
thewasif <wasif33@outlook.com>
2025-07-29 14:24:09 +0500

    Adding redux toolkit to project

--- react-indepth-learning/src/App.jsx
+++ react-indepth-learning/src/App.jsx
@@ -1,14 +1,20 @@
 import Chant from "./Chant";
 import Counter from "./components/Counter";
 import Card from "./components/Card";
+import Todos from "./components/Todos";
+import ViewTodos from "./components/ViewTodos";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Chant />
       <Counter />
       <Card username={"Bilal"} btnText="Click Me" />
       <Card username={"My Username"} />
-    </>
+      <Todos />
+      <ViewTodos />
+    </Provider>
   );
 }
 
